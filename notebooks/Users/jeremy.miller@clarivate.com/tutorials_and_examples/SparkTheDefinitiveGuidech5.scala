849bfb3968f2235744d313d0cdeef62e3d47a2e7
Jeremy Miller <jeremymiller00@gmail.com>
2020-03-10 08:55:23 -0700

    first commit

---
+++ notebooks/Users/jeremy.miller@clarivate.com/tutorials_and_examples/SparkTheDefinitiveGuidech5.scala
@@ -0,0 +1,28 @@
+// Databricks notebook source
+// MAGIC %md # Spark: The Definitive Guide
+
+// COMMAND ----------
+
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+// COMMAND ----------
+
+// MAGIC %md ## Chapter 5: Basic Structured Operations
+
+// COMMAND ----------
+
+// MAGIC %md All operations are translated and performed on *Spark* types, not Scala or Python types.
+
+// COMMAND ----------
+
+val df = spark.range(500).toDF("number")
+df.select(df.col("number") + 10)
+res0.show
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC df = spark.range(500).toDF("number")
+// MAGIC df.select(df['number'] + 10)
+// MAGIC df.show()
