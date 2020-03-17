6b796af95a39fdd3202650deb3ed62331bb1513a
Jeremy Miller <jeremymiller00@gmail.com>
2020-03-17 11:01:55 -0700

    first commit

---
+++ notebooks/Users/jeremy.miller@clarivate.com/tutorials_and_examples/SparkTheDefinitiveGuidech6.scala
@@ -0,0 +1,14 @@
+// Databricks notebook source
+// MAGIC %md # Spark: The Definitive Guide
+
+// COMMAND ----------
+
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+// COMMAND ----------
+
+// MAGIC %md ## Chapter 6: Working with Different Types of Data
+
+// COMMAND ----------
+
